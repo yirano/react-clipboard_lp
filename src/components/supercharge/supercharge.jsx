f7f7dffe9e29b76f20eba6380a2bb93b645b6d8b
yirano <yirano@protonmail.ch>
2019-12-08 22:20:24 -0500

    Wrap icons in card container

--- src/components/supercharge/supercharge.jsx
+++ src/components/supercharge/supercharge.jsx
@@ -1,4 +1,7 @@
 import React from 'react';
+import BlackList from '../../images/icon-blacklist.svg';
+import PlainText from '../../images/icon-text.svg';
+import Preview from '../../images/icon-preview.svg';
 import './supercharge.scss';
 
 export default function supercharge() {
@@ -7,6 +10,28 @@ export default function supercharge() {
 			<div className="topElement">
 				<h1>Supercharge your workflow</h1>
 				<p className="caption">We've got the tools to boost your productivity.</p>
+			</div>
+			<div className="secondElement">
+				<div className="card">
+					<img src={BlackList} alt="Create blacklists icon" />
+				</div>
+				<div className="card">
+					<img src={PlainText} alt="Plain text snippets icon" />
+				</div>
+				<div className="card">
+					<img src={Preview} alt="Sneak Preview icon" />
+				</div>
+			</div>
+			<div className="thirdElement">
+				<div className="card">
+					<img src={BlackList} alt="Create blacklists icon" />
+				</div>
+				<div className="card">
+					<img src={PlainText} alt="Plain text snippets icon" />
+				</div>
+				<div className="card">
+					<img src={Preview} alt="Sneak Preview icon" />
+				</div>
 			</div>
 
 			<div className="bottomElement" />
