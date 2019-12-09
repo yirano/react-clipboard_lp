14605191259c6bf652501ec6068b07b3a70ce2a8
yirano <yirano@protonmail.ch>
2019-12-08 20:12:42 -0500

    Add content and style Access section

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import Header from './components/header/header';
 import KeepTrack from './components/keepTrack/keepTrack';
+import AccessClipboard from './components/accessClipboard/accessClipboard';
 import './App.scss';
 
 function App() {
@@ -8,6 +9,7 @@ function App() {
 		<div className="App">
 			<Header />
 			<KeepTrack />
+			<AccessClipboard />
 		</div>
 	);
 }
---
+++ src/components/accessClipboard/accessClipboard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import './accessClipboard.scss';
+
+export default function accessClipboard() {
+	return (
+		<section className="accessClipboard">
+			<div className="topElement">
+				<h1>Access Clipboard anywhere</h1>
+				<p className="caption">
+					Whether you're on the go, or at your computer, you can access all your Cliboard snippets in a few
+					simple clicks.
+				</p>
+			</div>
+			<div className="bottomElement" />
+		</section>
+	);
+}
