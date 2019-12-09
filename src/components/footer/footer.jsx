0f4897f4d3625af714a91591d77c96c0971e6f37
yirano <yirano@protonmail.ch>
2019-12-09 17:51:54 -0500

    Add contents to footer

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,55 @@
 import React from 'react';
+import Logo from '../../images/logo.svg';
+import Facebook from '../../images/icon-facebook.svg';
+import Twitter from '../../images/icon-twitter.svg';
+import Instagram from '../../images/icon-instagram.svg';
 import './footer.scss';
 
 export default function footer() {
 	return (
 		<footer>
-			<h1>THIS IS THE FOOTER</h1>
+			<div className="firstColumn">
+				<img src={Logo} alt="Clipboard Logo" />
+			</div>
+			<div className="secondColumn">
+				<ul>
+					<li>
+						<a href="/">FAQs</a>
+					</li>
+					<li>
+						<a href="/">Privacy Policy</a>
+					</li>
+					<li>
+						<a href="/">Install Guide</a>
+					</li>
+					<li>
+						<a href="/">Contact Us</a>
+					</li>
+					<li>
+						<a href="/">Press Kit</a>
+					</li>
+				</ul>
+			</div>
+
+			<div className="thirdColumn">
+				<ul>
+					<li>
+						<a href="/">
+							<img src={Facebook} alt="Facebook" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src={Twitter} alt="Twitter" />
+						</a>
+					</li>
+					<li>
+						<a href="/">
+							<img src={Instagram} alt="Instagram" />
+						</a>
+					</li>
+				</ul>
+			</div>
 		</footer>
 	);
 }
