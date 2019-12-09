7eaabf06fa82931ae23dd23921911c7ccea8bdee
yirano <yirano@protonmail.ch>
2019-12-08 22:11:27 -0500

    Begin adding contents to Supercharge section

--- src/App.js
+++ src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Header from './components/header/header';
 import KeepTrack from './components/keepTrack/keepTrack';
 import AccessClipboard from './components/accessClipboard/accessClipboard';
+import Supercharge from './components/supercharge/supercharge';
 import './App.scss';
 
 function App() {
@@ -10,6 +11,7 @@ function App() {
 			<Header />
 			<KeepTrack />
 			<AccessClipboard />
+			<Supercharge />
 		</div>
 	);
 }
---
+++ src/components/supercharge/supercharge.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+export default function supercharge() {
+	return (
+		<section className="supercharge">
+			<div className="topElement">
+				<h1>Supercharge your workflow</h1>
+				<p className="caption">We've got the tools to boost your productivity.</p>
+			</div>
+
+			<div className="bottomElement" />
+		</section>
+	);
+}
