a0efc5d3e5341144a1d549aa2289a5d637587272
yirano <yirano@protonmail.ch>
2019-12-09 17:29:11 -0500

    Add contents to Clipboard section and begin styling

--- src/components/download/download.js
+++ src/components/download/download.js
@@ -4,7 +4,18 @@ import './download.scss';
 export default function download() {
 	return (
 		<section className="download">
-			<h1>DOWNLOAD SECTION</h1>
+			<div className="topElement">
+				<h1>Clipboard for iOS and Mac OS</h1>
+				<p className="caption">
+					Available for free on the App Store. Download for Mac or iOS, sync with iCloud and you're ready to
+					start adding to your clipboard.
+				</p>
+			</div>
+
+			<div className="bottomElement">
+				<button>Download for iOS</button>
+				<button>Download for Mac</button>
+			</div>
 		</section>
 	);
 }
